e9de2561008619cd72e9a97ad69decf8e801883b
Ghulam Shabbir <ghulamshabbirmtn500@gmail.com>
2023-12-10 18:12:25 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'everything-github-demo';
+  book: any = { };
+
+  constructor(http: HttpClient) {
+    http.get('https://api.angular.schule/book/9783864906466')
+      .subscribe(b => this.book = b);
+  }
 }
